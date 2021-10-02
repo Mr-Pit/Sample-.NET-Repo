16e44a62ab9475ba4ca2e05856973b0c5977a16c
Pitney Jules <PJules602@gmail.com>
2021-10-02 01:59:42 -0400

    Changed timing

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            System.Threading.Thread.Sleep(10000);
+            System.Threading.Thread.Sleep(30000);
         }
     }
 }
